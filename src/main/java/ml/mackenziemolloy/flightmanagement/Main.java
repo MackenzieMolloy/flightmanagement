2d25efa67692405e5ae5287ceecc9c90f5e899c3
Mackenzie Molloy <48184419+MackenzieMolloy@users.noreply.github.com>
2020-08-14 07:25:46 +0100

    Update Main.java
    Added comments to (vaguely) explain the code

--- src/main/java/ml/mackenziemolloy/flightmanagement/Main.java
+++ src/main/java/ml/mackenziemolloy/flightmanagement/Main.java
@@ -7,23 +7,28 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 
 public class Main extends JavaPlugin {
-
+    
+    // Generates, and sets-up the config.yml file utilizing the config class
     private final SettingsManager settingsManager = SettingsManagerBuilder
             .withYamlFile(new File(getDataFolder(),"config.yml"))
             .configurationData(Config.class)
             .useDefaultMigrationService()
             .create();
 
+    // On server startup
     @Override
     public void onEnable() {
+        // Registers the command class
         new Commands(this);
 
+        // Outputs a plugin startup message to console
         this.getLogger().info("-*-");
         this.getLogger().info("Flight Management");
         this.getLogger().info("Made by Mackenzie Molloy");
         this.getLogger().info("-*-");
     }
 
+    // Creates an accessable config data getter
     public SettingsManager getSettings() {
         return settingsManager;
     }
